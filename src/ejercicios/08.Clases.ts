dc0fbb3b832c196a48f88b7f45f549941d1e5697
bmonroyg <bmonroyg@emeal.nttdata.com>
2023-05-27 13:29:29 -0500

    seccion 3

--- src/ejercicios/08.Clases.ts
+++ src/ejercicios/08.Clases.ts
@@ -11,6 +11,8 @@ class Heroe extends PersonaNormal{
 // edad:number;
 // nombreReal:string;
 
+public person: PersonaNormal;
+
 constructor( 
 	public alterEgo:string,
 	public edad:number, 
@@ -19,6 +21,9 @@ constructor(
 	}
 }
 
+const tony = new Heroe('Ironman', 45, 'Tony Stark');
 const spiderMan = new Heroe('Spiderman', 30, 'Peter Parker');
 
+console.log(tony);
 console.log(spiderMan);
+console.log(tony.person);
